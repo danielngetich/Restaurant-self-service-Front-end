13fb35b630f68924560657ebf7a702b7f37ca977
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-12-09 12:22:24 +0300

    add routes

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -15,6 +15,7 @@ function Admin() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        'Accept': 'application/json'
       },
       body: JSON.stringify(admin),
     })
